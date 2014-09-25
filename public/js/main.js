a19b3c367bc317aee7516d8571d5119714eb1a6e
Guillermo Hernán De Souza <hds@fansworld.tv>
2014-09-25 17:16:21 -0300

    some updates

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,10 @@
 (function ($) {
   $(document).ready(function () {
     // you awesome code goes here
+    var myLzld = lazyload({
+	  container: document.body,
+	  offset: 333,
+	  src: 'data-src' // or function(elt) { return customSrc }
+	});
   });
 })(jQuery);
