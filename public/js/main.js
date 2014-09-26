ed3b5d3826fb488c775935a0246784c63bcea0ec
Guillermo Hernán De Souza <hds@fansworld.tv>
2014-09-26 15:34:53 -0300

    sfw downloaded

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,49 @@
 var sfwList = [];
+var titleList = [];
+var btnsList = [];
 
 (function ($) {
   $(document).ready(function () {
     // you awesome code goes here
+    /*
     var myLzld = lazyload({
 	  container: document.body,
 	  offset: 333,
 	  src: 'data-src' // or function(elt) { return customSrc }
 	});
+  	*/
     
 	
-	$( "iframe" ).each(function() {
-	  var url = $( this ).attr( "data-src" );
+	$( 'iframe' ).each(function() {
+	  var url = $( this ).attr( 'data-src' );
 	  sfwList.push(url);
-	  $( this ).before('<a href="' + url + '" download>download</a>');
+	  //$( this ).before('<a href="' + url + '" download>download</a>');
 	});
 
+	$( 'span.title' ).each(function() {
+	  var title = $( this ).text();
+	  titleList.push(title);
+	  //$( this ).before('<a href="' + url + '" download>download</a>');
+	});
+
+	/*
+	for (i=0; i < sfwList.length; i++){
+		var title = titleList[i];
+		var url = sfwList[i];
+		btnsList.push({ 'title': title, 'url': url });
+	}
+	*/
+	/*
+	for (i=0; i < sfwList.length; i++){
+		var url = sfwList[i];
+		var link = $('<a class="sfwDownload" href="' + url +'" download>download</a><br/>');
+		$('body').prepend(link);
+	}
+
+	for (i=0; i < sfwList.length; i++){
+		var url = sfwList[i];
+		window.open(url);
+	}
+	*/
   });
 })(jQuery);
